296c8fb77eba64a2f31d14981165758a5315e526
KE MA <ke.ma@qhrtech.com>
2018-12-03 11:27:28 -0500

    Initial Commit

---
+++ DeselectTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeselectTest
+//
+//  Created by Ke Ma on 2018-12-03.
+//  Copyright © 2018 Ke Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
